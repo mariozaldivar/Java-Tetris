b7d7501bc6f250cbc0ec699977b2abfd28a48cb0
mariozaldivar <mario.zaldivardlc@gmail.com>
2026-03-03 22:12:18 -0600

    Se añadió la funcionalidad básica de Clock, las demás clases pueden reconocer el Tick

--- src/Board.java
+++ src/Board.java
@@ -1,4 +1,14 @@
 public class Board {
 
+    void probarTick() {
+        System.out.println("La clase Board está reconociendo el Tick");
+    }
+    public void main(String[] args) {
+    Clock.INSTANCE.suscribe(this::probarTick);
+    Clock.INSTANCE.updateClock(1000);
+    while (true) {
+            assert true;
+        }
+    }
 
 }
--- src/Clock.java
+++ src/Clock.java
@@ -1,7 +1,7 @@
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.*;
+
 
 // Definición de clase Clock que usa arquitectura Singleton
 // Enum se asegura de que solo se pueda crear una instancia en todo el código
@@ -9,10 +9,28 @@ public enum Clock {
     INSTANCE;
 
     private boolean playing = true;
+    private boolean isPaused = false;
+    private final ScheduledExecutorService scheduler = Executors.newSingleThreadScheduledExecutor();
+    // Esta clase crea un scheduler, que se encarga de revisar el paso del tiempo
+    // El nombre es largo y parece confusa, pero es importante para la lógica del clock
+    private ScheduledFuture<?> currentTickTask;
+    private long speed = 16;
 
-
+    public void pauseGame() {
+        isPaused = true;
+    }
+    public void unpauseGame() {
+        isPaused = false;
+    }
     public void stopPlaying() {
         playing = false;
+    }
+    public void updateClock(long newSpeed) {
+        if (currentTickTask != null && !currentTickTask.isCancelled())
+        {
+            currentTickTask.cancel(false);
+        }
+        currentTickTask = scheduler.scheduleAtFixedRate(this::tick, 0, newSpeed, TimeUnit.MILLISECONDS );
     }
 
     // Lista con todos los métodos suscritos al evento Tick();
@@ -23,22 +41,13 @@ public enum Clock {
     }
 
     private void tick() {// Evento Tick
-        tickListeners.forEach(Runnable::run);
-    }
+        if (isPaused) { return; }
+        else {
 
-    private void Update() { // Función donde se almacenan todas las cosas que deben estar sucendiendo cada frame
-        while (playing) {
-
+            tickListeners.forEach(Runnable::run);
         }
-
     }
 
-
-    void Main() {
-
-        Update();
-
-    }
 
 }
 
@@ -53,4 +62,3 @@ public enum Clock {
 
 
 
-}
