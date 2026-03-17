c1682eb6146d3b45e87c656e8d94361a77eccbb0
mariozaldivar <mario.zaldivardlc@gmail.com>
2026-03-17 09:42:05 -0600

    Se añadió forma de las piezas

--- src/Clock.java
+++ src/Clock.java
@@ -71,6 +71,15 @@ public enum Clock {
     }
 
 
+
+    public void printIntMatrix(int[][] matrix){
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                System.out.print(matrix[i][j]);
+            }
+            System.out.println();
+        }
+    }
 }
 
 
--- src/Piece.java
+++ src/Piece.java
@@ -3,16 +3,65 @@ import java.util.Random;
 
 public class Piece {
 
-    public ArrayList<int[]> shape;
+    public int[][] shape;
     Random random = new Random();
+    public int[][][] allShapes =
+            {
+                    {
+                        {1, 1},
+                        {1, 1}
+                    },
 
-    public void rotate()
+
+                    {
+                            {0, 0, 0, 0},
+                            {1, 1, 1, 1},
+                            {0, 0, 0, 0},
+                            {0, 0, 0, 0}
+                    },
+
+                    {
+                            {0, 0, 0},
+                            {1, 1, 0},
+                            {0, 1, 1}
+                    },
+
+                    {
+                            {0, 0, 0},
+                            {0, 1, 1},
+                            {1, 1, 0}
+                    },
+                    {
+                            {0, 0, 0},
+                            {1, 0, 0},
+                            {1, 1, 1}
+                    },
+                    {
+                            {0, 0, 0},
+                            {0, 0, 1},
+                            {1, 1, 1}
+                    },
+                    {
+                            {0, 0, 0},
+                            {0, 1, 0},
+                            {1, 1, 1}
+                    },
+
+            };
+
+    Piece() {
+        int newSelect = random.nextInt(7);
+        this.shape = this.allShapes[newSelect];
+    }
+
+    public void rotate() {
+
+    }
+
+    public void main()
     {
 
     }
 
-    Piece() {
-
-    }
 
 }
