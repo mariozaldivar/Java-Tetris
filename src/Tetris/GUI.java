c7a4ab4cd4ca4aec28f48b459db9a6d912c22dfc
Rfcc22 <rfconchac@gmail.com>
2026-03-17 11:37:37 -0600

    Añadi parte de la pantalla de inicio

--- src/Tetris/GUI.java
+++ src/Tetris/GUI.java
@@ -26,17 +26,58 @@ public class GUI extends Application { // GUI quiere decir Graphical User Interf
     public void start(Stage stage) throws Exception {
         //PantallaInicio
         BorderPane PantallaInicio = new BorderPane();
-        Scene scene = new Scene(PantallaInicio,1000,1000,Color.WHITE); //Scene es lo que va a aparecer en la pantalla es Scene(nodo,x,y,Colordefondo) xy se mide en pixeles
+        Scene scene = new Scene(PantallaInicio,Color.WHITE); //Scene es lo que va a aparecer en la pantalla es Scene(nodo,x,y,Colordefondo) xy se mide en pixeles
         Image Logo = new Image("LogoTetris.png"); //Es el logo que aparece en la ventana del juego
 
-        VBox BotonesIncio = new VBox(20);
+        VBox BotonesIncio = new VBox(20);//Vbox (espacio entre hijos en px)
 
         Button IniciarJuego = new Button();
-
-        //test
-
         Button Configuracion = new Button();
         Button Salir = new Button();
+
+        IniciarJuego.setPrefSize(400,75); //(Largo, alto)
+        Configuracion.setPrefSize(400,75);
+
+        IniciarJuego.setOnMouseEntered(event -> { //Suscede algoc cuando el mouse oasa por enciam del boton
+            IniciarJuego.setStyle("-fx-background-color: #22bfa1;");
+        } );
+        IniciarJuego.setOnMouseExited(event -> { //sucede algo cuando el
+            IniciarJuego.setStyle("-fx-background-color: #ffffff;");
+        });
+        IniciarJuego.setOnMouseClicked(event -> {
+            System.out.println("Iniciar clicked");
+            IniciarJuego.setStyle("-fx-background-color: #51b624;");
+
+        });
+        IniciarJuego.setOnMouseReleased(event ->{
+            System.out.println("Iniciar Released");
+            IniciarJuego.setStyle("-fx-background-color: #ffffff;");
+        });
+
+
+        Configuracion.setOnMouseEntered(event -> {//Sucede algo cuando el mouse pasa por encima del boton
+            Configuracion.setStyle("-fx-background-color: #22bfa1;");
+        });
+        Configuracion.setOnMouseExited(event -> {//Sucede algo cuando el mouse deja de pasar por encima del boton
+            Configuracion.setStyle("-fx-background-color: #ffffff;");
+        });
+        Configuracion.setOnMouseClicked(event -> {//Sucede algo cuando presionas el mouse
+            System.out.println("Configuracion clicked");
+            Configuracion.setStyle("-fx-background-color: #3ac129;");
+        });
+        Configuracion.setOnMouseReleased(event -> {//Sucede algo cuando dejas de presionar el mouse
+            System.out.println("Configuracion released");
+            Configuracion.setStyle("-fx-background-color: #ffffff;");
+        });
+
+        BotonesIncio.getChildren().addAll(IniciarJuego,Configuracion);
+        PantallaInicio.setCenter(BotonesIncio);
+        BotonesIncio.setAlignment(Pos.CENTER);
+        BorderPane.setAlignment(BotonesIncio,Pos.CENTER);
+
+
+
+
 
 
 
@@ -49,7 +90,7 @@ public class GUI extends Application { // GUI quiere decir Graphical User Interf
 
 
 
-
+/**
         GridPane TableroTetris = new GridPane(); //Gridpane es para hacer el tablero de tetris
         TableroTetris.setAlignment(Pos.CENTER); //Hace que todos los nodo de Gripanes se dibujen centrados
         for (int rows = 0; rows < 20; rows++){ //Dos for para hacer que el tablero tenga 20 filas y 10 columnas
@@ -73,12 +114,14 @@ public class GUI extends Application { // GUI quiere decir Graphical User Interf
             System.out.println("No hay teclas presionadas");
         });
 
-        PantallaInicio.setCenter(TableroTetris); //Coloca a el tablero en el centro de la pantalla
+        //PantallaInicio.setCenter(TableroTetris); //Coloca a el tablero en el centro de la pantalla
+        **/
 
-        stage.setFullScreen(true); //Pone en patalla completa el juego
+        //stage.setFullScreen(true); //Pone en patalla completa el juego
         stage.setTitle("Tetris");
         stage.getIcons().add(Logo);
         stage.setScene(scene);
+        stage.setMaximized(true);
         stage.show(); //Muestras la ventana
 
     }
