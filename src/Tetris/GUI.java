1ee5e0f514ae9dc72a86ea624992d9ce4bd6628c
Rfcc22 <rfconchac@gmail.com>
2026-03-22 22:57:06 -0600

    Añadi parte de la pantalla de inicio

--- src/Tetris/GUI.java
+++ src/Tetris/GUI.java
@@ -3,79 +3,59 @@ package Tetris;
 import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.*;
+import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import javafx.scene.paint.Color;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyCombination;
-import javafx.scene.layout.GridPane;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.layout.BorderPane;
+
+import java.util.concurrent.ThreadPoolExecutor;
 
 public class GUI extends Application { // GUI quiere decir Graphical User Interface
 
     public static void main(String[] args){
         launch(args);
     } //Main temporal para correr la GUI
+
+    Image Logo = new Image("LogoTetris.png"); //Es el logo que aparece en la ventana del juego
+
+    //Variables de la Pantalla de inicio
+    BorderPane PantallaInicio = new BorderPane();
+    Scene scene = new Scene(PantallaInicio,Color.WHITE); //Scene es lo que va a aparecer en la pantalla es Scene(nodo,x,y,Colordefondo) xy se mide en pixeles
+    VBox BotonesIncio = new VBox(20);//Vbox (espacio entre hijos en px)
+    Button IniciarJuego = new Button();
+    Button Configuracion = new Button();
+
+    Text Titulo = new Text("Tetris");
+
     @Override
     public void start(Stage stage) throws Exception {
         //PantallaInicio
-        BorderPane PantallaInicio = new BorderPane();
-        Scene scene = new Scene(PantallaInicio,Color.WHITE); //Scene es lo que va a aparecer en la pantalla es Scene(nodo,x,y,Colordefondo) xy se mide en pixeles
-        Image Logo = new Image("LogoTetris.png"); //Es el logo que aparece en la ventana del juego
+        BorderPane EscenaJuego = new BorderPane();
+        BorderPane EscenaConfiguracion = new BorderPane();
+        BorderPane Gameover = new BorderPane();
 
-        VBox BotonesIncio = new VBox(20);//Vbox (espacio entre hijos en px)
-
-        Button IniciarJuego = new Button();
-        Button Configuracion = new Button();
-        Button Salir = new Button();
-
-        IniciarJuego.setPrefSize(400,75); //(Largo, alto)
-        Configuracion.setPrefSize(400,75);
-
-        IniciarJuego.setOnMouseEntered(event -> { //Suscede algoc cuando el mouse oasa por enciam del boton
-            IniciarJuego.setStyle("-fx-background-color: #22bfa1;");
-        } );
-        IniciarJuego.setOnMouseExited(event -> { //sucede algo cuando el
-            IniciarJuego.setStyle("-fx-background-color: #ffffff;");
-        });
-        IniciarJuego.setOnMouseClicked(event -> {
-            System.out.println("Iniciar clicked");
-            IniciarJuego.setStyle("-fx-background-color: #51b624;");
-
-        });
-        IniciarJuego.setOnMouseReleased(event ->{
-            System.out.println("Iniciar Released");
-            IniciarJuego.setStyle("-fx-background-color: #ffffff;");
-        });
+        SetupMenuButtons(IniciarJuego,"Iniciar",scene,EscenaJuego);
+        SetupMenuButtons(Configuracion,"Configuracion",scene,EscenaConfiguracion);
 
 
-        Configuracion.setOnMouseEntered(event -> {//Sucede algo cuando el mouse pasa por encima del boton
-            Configuracion.setStyle("-fx-background-color: #22bfa1;");
-        });
-        Configuracion.setOnMouseExited(event -> {//Sucede algo cuando el mouse deja de pasar por encima del boton
-            Configuracion.setStyle("-fx-background-color: #ffffff;");
-        });
-        Configuracion.setOnMouseClicked(event -> {//Sucede algo cuando presionas el mouse
-            System.out.println("Configuracion clicked");
-            Configuracion.setStyle("-fx-background-color: #3ac129;");
-        });
-        Configuracion.setOnMouseReleased(event -> {//Sucede algo cuando dejas de presionar el mouse
-            System.out.println("Configuracion released");
-            Configuracion.setStyle("-fx-background-color: #ffffff;");
-        });
 
-        BotonesIncio.getChildren().addAll(IniciarJuego,Configuracion);
+
+        Titulo.setFont(new Font("Impact",70));
+
+        PantallaInicio.setTop(Titulo);
+        BotonesIncio.getChildren().addAll(Titulo,IniciarJuego,Configuracion);
         PantallaInicio.setCenter(BotonesIncio);
         BotonesIncio.setAlignment(Pos.CENTER);
         BorderPane.setAlignment(BotonesIncio,Pos.CENTER);
-
-
 
 
 
@@ -123,6 +103,31 @@ public class GUI extends Application { // GUI quiere decir Graphical User Interf
         stage.setScene(scene);
         stage.setMaximized(true);
         stage.show(); //Muestras la ventana
+    }
+
+
+    private void SetupMenuButtons (Button button, String ButtonName, Scene scene, BorderPane changeroot){
+        button.setPrefSize(400,75);
+        button.setOnMouseEntered(event -> button.setStyle("-fx-background-color: #22bfa1;"));
+        button.setOnMouseExited(event -> button.setStyle("-fx-background-color: #ffffff;"));
+
+        button.setOnMouseClicked(event -> {
+            System.out.println("Se ha presionado" + ButtonName);
+            button.setStyle("-fx-background-color: #3ac129;");
+            scene.setRoot(changeroot);
+
+        });
+        button.setOnMouseReleased(mouseEvent -> {
+            System.out.println("Se ha presionado " + ButtonName);
+            button.setStyle("-fx-background-color: #ffffff;");
+        });
+    }
+
+    private void SetupPantallaInicio() {
 
     }
+
+
+
 }
+
