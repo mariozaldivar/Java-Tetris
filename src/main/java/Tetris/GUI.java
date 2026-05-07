bbc0b9c67b88aa3c99d26d341aa6b05aad2d3e33
mariozaldivar <mario.zaldivardlc@gmail.com>
2026-05-07 09:31:37 -0600

    Se añadió Main.java, y primera implementación funcional de Maven Shade para crear el FAT Jar

--- src/main/java/Tetris/GUI.java
+++ src/main/java/Tetris/GUI.java
@@ -48,26 +48,24 @@ public class GUI extends Application {
 
   Game updateGame = new Game();
 
-
   private static final int cellWidht = 30;
   private static final int cellHeight = 30;
   GridPane Tablero = new GridPane();
   Node[][] Cellmap = new Node[BOARD_HEIGHT][BOARD_WIDTH];
 
-
-  //Configuracion del tablero
-  private void CrearTablero(){
+  // Configuracion del tablero
+  private void CrearTablero() {
     Tablero.setAlignment(Pos.CENTER);
-    Tablero.setMaxSize(Region.USE_PREF_SIZE,Region.USE_PREF_SIZE);
-    for (int rows = 0; rows < BOARD_HEIGHT; rows++){
-      for (int colums = 0; colums < BOARD_WIDTH; colums++){
-        Rectangle Cell = new Rectangle(cellWidht,cellHeight);
-        if (rows < 2){
+    Tablero.setMaxSize(Region.USE_PREF_SIZE, Region.USE_PREF_SIZE);
+    for (int rows = 0; rows < BOARD_HEIGHT; rows++) {
+      for (int colums = 0; colums < BOARD_WIDTH; colums++) {
+        Rectangle Cell = new Rectangle(cellWidht, cellHeight);
+        if (rows < 2) {
           Cell.setOpacity(.20);
           Cell.setStroke(Color.BLACK);
           Cell.setStrokeWidth(1);
         }
-        Tablero.add(Cell,colums,rows);
+        Tablero.add(Cell, colums, rows);
         Cellmap[rows][colums] = Cell;
         Cell.setStroke(Color.BLACK);
         Cell.setStrokeWidth(1);
@@ -76,10 +74,10 @@ public class GUI extends Application {
     Tablero.setStyle("-fx-border-color: #808080; -fx-border-width: 5px; -fx-border-style: solid;");
   }
 
-  private void updateTablero(){
+  private void updateTablero() {
     int[][] tableroActual = updateGame.getBoardState();
-    for(int rows = 0; rows < BOARD_HEIGHT; rows++){
-      for (int colums = 0; colums < BOARD_WIDTH; colums++){
+    for (int rows = 0; rows < BOARD_HEIGHT; rows++) {
+      for (int colums = 0; colums < BOARD_WIDTH; colums++) {
         Rectangle currentCell = (Rectangle) Cellmap[rows][colums];
         int numeroCelda = tableroActual[rows][colums];
         currentCell.setFill(setColor(numeroCelda));
@@ -88,7 +86,7 @@ public class GUI extends Application {
     }
   }
 
-  private static  Color setColor(int numeroCelda) {
+  private static Color setColor(int numeroCelda) {
     return switch (numeroCelda) {
       case 0 -> Color.WHITE;
       case 1 -> Color.RED;
@@ -102,14 +100,15 @@ public class GUI extends Application {
     };
   }
 
-  //Botones
-  private void hoverOverButton(Button button){
-    button.setPrefSize(400,75);
+  // Botones
+  private void hoverOverButton(Button button) {
+    button.setPrefSize(400, 75);
     button.setOnMouseEntered(event -> button.setStyle("-fx-background-color: #22bfa1;"));
     button.setOnMouseExited(event -> button.setStyle("-fx-background-color: #ffffff;"));
   }
 
-  private void setupMenuButton(Button button, Scene tetris, BorderPane cambiarEscena, String nombre, Font font, Parent escenaAnterior){
+  private void setupMenuButton(Button button, Scene tetris, BorderPane cambiarEscena, String nombre, Font font,
+      Parent escenaAnterior) {
     button.setFont(font);
     button.setText(nombre);
     hoverOverButton(button);
@@ -121,7 +120,7 @@ public class GUI extends Application {
         tetris.setRoot(cambiarEscena);
       }
 
-      if (nombre.equals("INICIAR")){
+      if (nombre.equals("INICIAR")) {
         System.out.println("Se ha presionado " + nombre);
         updateTableroTetris();
         Clock.INSTANCE.startGame();
@@ -133,42 +132,45 @@ public class GUI extends Application {
     button.setOnMouseReleased(event -> button.setStyle("-fx-background-color: #ffffff;"));
   }
 
-  private void setupConfigButtons(VBox keybindsUI, Map<String, KeyCode> keybinds, Font font){
+  private void setupConfigButtons(VBox keybindsUI, Map<String, KeyCode> keybinds, Font font) {
     keybindsUI.getChildren().clear();
-    keybindsUI.setPadding(new Insets(50, 0 ,50, 0));
+    keybindsUI.setPadding(new Insets(50, 0, 50, 0));
     GridPane configLayout = new GridPane();
     configLayout.setHgap(30);
     configLayout.setVgap(15);
     configLayout.setAlignment(Pos.CENTER);
 
     int row = 0;
-    for (String keyName : keybinds.keySet()){ //Añade cada boton de la configuracion
+    for (String keyName : keybinds.keySet()) { // Añade cada boton de la configuracion
       Label label = new Label(keyName);
       label.setStyle("-fx-text-fill: black;");
       label.setFont(font);
 
       Button button = new Button(keybinds.get(keyName).toString());
-      button.setPrefSize(400,50);
+      button.setPrefSize(400, 50);
       button.setFont(font);
       hoverOverButton(button);
-      button.setOnMouseClicked(event -> { //El texto dentro del button cambia a ...
+      button.setOnMouseClicked(event -> { // El texto dentro del button cambia a ...
         button.requestFocus();
         button.setText("...");
 
-        button.focusedProperty().addListener((ObservableValue,OldValue,NewValue) ->{ //Cuando seleccionas un nuevo boton o haces click en otro lugar de la pantalla el boton regresa a su estado original
-          if (!NewValue){
+        button.focusedProperty().addListener((ObservableValue, OldValue, NewValue) -> { // Cuando seleccionas un nuevo
+                                                                                        // boton o haces click en otro
+                                                                                        // lugar de la pantalla el boton
+                                                                                        // regresa a su estado original
+          if (!NewValue) {
             button.setText(keybinds.get(keyName).toString());
             button.setOnKeyPressed(null);
           }
         });
 
-        button.setOnKeyPressed(e -> { //Coloca la nueva tecla a la accion deseada
+        button.setOnKeyPressed(e -> { // Coloca la nueva tecla a la accion deseada
           KeyCode currentkey = e.getCode();
-          if (currentkey == KeyCode.ESCAPE){
+          if (currentkey == KeyCode.ESCAPE) {
             button.setText(keybinds.get(keyName).toString());
             button.setOnKeyPressed(null);
           }
-          if (keybinds.containsValue(currentkey)){
+          if (keybinds.containsValue(currentkey)) {
             button.setText("Key already in use");
             PauseTransition wait = new PauseTransition(Duration.seconds(1));
             wait.setOnFinished(Actionevent -> button.setText(keybinds.get(keyName).toString()));
@@ -181,8 +183,8 @@ public class GUI extends Application {
           e.consume();
         });
       });
-      configLayout.add(label,0,row);
-      configLayout.add(button,1,row);
+      configLayout.add(label, 0, row);
+      configLayout.add(button, 1, row);
       row++;
     }
     keybindsUI.getChildren().add(configLayout);
@@ -194,38 +196,38 @@ public class GUI extends Application {
     int[][] holdPieceMatrix = updateGame.holdPiece.shape;
     int holdPieceSize = updateGame.holdPiece.size;
 
-    int offset = (4 - holdPieceSize)/2;
+    int offset = (4 - holdPieceSize) / 2;
 
-    for(int rows = 0; rows < holdPieceSize; rows++){
-      for (int colums = 0; colums < holdPieceSize; colums++){
-        int currentRectangle =  holdPieceMatrix[rows][colums];
-        if (currentRectangle > 0){
-          Rectangle rect = new  Rectangle(cellWidht,cellHeight);
+    for (int rows = 0; rows < holdPieceSize; rows++) {
+      for (int colums = 0; colums < holdPieceSize; colums++) {
+        int currentRectangle = holdPieceMatrix[rows][colums];
+        if (currentRectangle > 0) {
+          Rectangle rect = new Rectangle(cellWidht, cellHeight);
           rect.setFill(setColor(currentRectangle));
 
-          HoldPiece.add(rect,colums + offset,rows  + offset);
+          HoldPiece.add(rect, colums + offset, rows + offset);
         }
 
       }
     }
   }
 
-  private void resetKeybinds(VBox keybindsUI, Map<String, KeyCode> keybinds){
+  private void resetKeybinds(VBox keybindsUI, Map<String, KeyCode> keybinds) {
 
   }
 
-  private void pauseMenu(){}
-
-
+  private void pauseMenu() {
+  }
 
   public void start(Stage currentScene) throws Exception {
-    Font tetrisfontTitulo = Font.loadFont(getClass().getResource("/fonts/PressStart2P-Regular.ttf").toExternalForm(), 80);
+    Font tetrisfontTitulo = Font.loadFont(getClass().getResource("/fonts/PressStart2P-Regular.ttf").toExternalForm(),
+        80);
     Font textoBotones = Font.loadFont(getClass().getResource("/fonts/PressStart2P-Regular.ttf").toExternalForm(), 20);
-    Font configuracionTexto = Font.loadFont(getClass().getResource("/fonts/PressStart2P-Regular.ttf").toExternalForm(), 30);
+    Font configuracionTexto = Font.loadFont(getClass().getResource("/fonts/PressStart2P-Regular.ttf").toExternalForm(),
+        30);
 
-
-    //Menu principal
-    Image Background =  new Image(getClass().getResourceAsStream("/images/Mainmenu.gif"));
+    // Menu principal
+    Image Background = new Image(getClass().getResourceAsStream("/images/Mainmenu.gif"));
     ImageView background = new ImageView(Background);
 
     StackPane rootcontainer = new StackPane();
@@ -242,7 +244,7 @@ public class GUI extends Application {
     background.setPreserveRatio(true);
     background.setSmooth(true);
 
-    rootcontainer.getChildren().addAll(background,menuPrincipal);
+    rootcontainer.getChildren().addAll(background, menuPrincipal);
 
     VBox botonesInicio = new VBox();
 
@@ -252,16 +254,16 @@ public class GUI extends Application {
     tituloMenuPrincipal.setFill(Color.WHITE);
     tituloMenuPrincipal.setFont(tetrisfontTitulo);
 
-    setupMenuButton(iniciarJuego, pantallaTetris, menuJuego, "INICIAR", textoBotones,rootcontainer);
-    setupMenuButton(Configuracion, pantallaTetris, menuConfiguracion, "CONFIG",textoBotones,rootcontainer);
+    setupMenuButton(iniciarJuego, pantallaTetris, menuJuego, "INICIAR", textoBotones, rootcontainer);
+    setupMenuButton(Configuracion, pantallaTetris, menuConfiguracion, "CONFIG", textoBotones, rootcontainer);
 
-    botonesInicio.getChildren().addAll(tituloMenuPrincipal,iniciarJuego,Configuracion);
+    botonesInicio.getChildren().addAll(tituloMenuPrincipal, iniciarJuego, Configuracion);
     botonesInicio.setSpacing(20);
 
     menuPrincipal.setCenter(botonesInicio);
     botonesInicio.setAlignment(Pos.CENTER);
 
-    //Configuracion
+    // Configuracion
     VBox ConfigUI = new VBox();
     ConfigUI.setAlignment(Pos.CENTER);
     ConfigUI.setSpacing(20);
@@ -271,18 +273,18 @@ public class GUI extends Application {
     tituloConfiguracion.setFont(tetrisfontTitulo);
 
     Map<String, KeyCode> keybinds = new HashMap<>();
-    keybinds.put("Left",KeyCode.LEFT);
-    keybinds.put("Right",KeyCode.RIGHT);
+    keybinds.put("Left", KeyCode.LEFT);
+    keybinds.put("Right", KeyCode.RIGHT);
     keybinds.put("Soft drop", KeyCode.DOWN);
     keybinds.put("Hard drop", KeyCode.SPACE);
     keybinds.put("Rotate", KeyCode.X);
     keybinds.put("Rotate Counterclockwise", KeyCode.Z);
-    keybinds.put("rotate 180",KeyCode.A);
-    keybinds.put("Hold",KeyCode.C);
+    keybinds.put("rotate 180", KeyCode.A);
+    keybinds.put("Hold", KeyCode.C);
 
     setupConfigButtons(ConfigUI, keybinds, configuracionTexto);
     ConfigUI.getChildren().addFirst(tituloConfiguracion);
-    setupMenuButton(back, pantallaTetris, menuPrincipal , "BACK", textoBotones,rootcontainer);
+    setupMenuButton(back, pantallaTetris, menuPrincipal, "BACK", textoBotones, rootcontainer);
     ConfigUI.getChildren().add(back);
 
     menuConfiguracion.setBottom(back);
@@ -290,37 +292,33 @@ public class GUI extends Application {
 
     scrollConfig.setContent(ConfigUI);
     scrollConfig.setFitToWidth(true);
-    scrollConfig.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER); //Evita el scroll horizontal
+    scrollConfig.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER); // Evita el scroll horizontal
     scrollConfig.setStyle("-fx-background-color:transparent; -fx-background: transparent;");
     smoothScroll(scrollConfig);
 
-    //Juego
+    // Juego
     VBox tableroDerecho = new VBox();
     VBox tableroIzquierdo = new VBox();
     GridPane HoldPiece = new GridPane();
 
     BorderPane posicionHold = new BorderPane();
 
-
-    for (int i = 0; i <4; i++){
-      HoldPiece.getColumnConstraints().add( new ColumnConstraints(cellWidht));
+    for (int i = 0; i < 4; i++) {
+      HoldPiece.getColumnConstraints().add(new ColumnConstraints(cellWidht));
       HoldPiece.getRowConstraints().add(new RowConstraints(cellHeight));
     }
     HoldPiece.setAlignment(Pos.CENTER_RIGHT);
-    HoldPiece.setPrefSize((cellWidht*4),(cellHeight*4));
-
+    HoldPiece.setPrefSize((cellWidht * 4), (cellHeight * 4));
 
     tableroIzquierdo.setAlignment(Pos.CENTER);
     tableroIzquierdo.setPadding(new Insets(20));
-    tableroIzquierdo .setPrefWidth(0);
+    tableroIzquierdo.setPrefWidth(0);
 
-    Label holdLabel =  new Label("HOLD");
+    Label holdLabel = new Label("HOLD");
     holdLabel.setFont(textoBotones);
-    tableroIzquierdo.getChildren().addAll(holdLabel,HoldPiece);
+    tableroIzquierdo.getChildren().addAll(holdLabel, HoldPiece);
     posicionHold.setRight(tableroIzquierdo);
     menuJuego.setLeft(posicionHold);
-
-
 
     CrearTablero();
     menuJuego.setCenter(Tablero);
@@ -336,36 +334,37 @@ public class GUI extends Application {
       KeyCode currentKey = event.getCode();
       System.out.println(event.getCharacter());
       String curkey = "";
-      for (Map.Entry<String, KeyCode> keybind : keybinds.entrySet()){ //Obtienes el nombre de la tecla presionada
-        if(keybind.getValue() == currentKey){
+      for (Map.Entry<String, KeyCode> keybind : keybinds.entrySet()) { // Obtienes el nombre de la tecla presionada
+        if (keybind.getValue() == currentKey) {
           curkey = keybind.getKey();
           break;
         }
       }
 
-      switch (curkey){
-        case "Left" -> updateGame.movePiece(0,-1);
-        case "Right" -> updateGame.movePiece(0,1);
+      switch (curkey) {
+        case "Left" -> updateGame.movePiece(0, -1);
+        case "Right" -> updateGame.movePiece(0, 1);
         case "Hard drop" -> updateGame.hardDrop();
         case "Rotate" -> updateGame.pieceRotate();
         case "Hold" -> {
-                updateGame.holdPiece();
-                showHoldPiece(HoldPiece);
+          updateGame.holdPiece();
+          showHoldPiece(HoldPiece);
         }
         default -> System.out.println("Se ha presionado la tecla" + currentKey);
       }
     });
   }
 
-  private void smoothScroll(ScrollPane scrollPane){ //Hace que el scroll sea más suave
-    double scrollSpeed = 0.1; //Duracion del scroll en s
+  private void smoothScroll(ScrollPane scrollPane) { // Hace que el scroll sea más suave
+    double scrollSpeed = 0.1; // Duracion del scroll en s
 
     scrollPane.getContent().setOnScroll(event -> {
       double deltaY = event.getDeltaY() * scrollSpeed;
       double target = scrollPane.getVvalue() - deltaY;
 
       Timeline timeline = new Timeline();
-      KeyValue keyValue = new KeyValue( scrollPane.vvalueProperty(), Math.max(0,Math.min(1,target)), Interpolator.EASE_OUT);
+      KeyValue keyValue = new KeyValue(scrollPane.vvalueProperty(), Math.max(0, Math.min(1, target)),
+          Interpolator.EASE_OUT);
       KeyFrame kf = new KeyFrame(Duration.millis(200), keyValue);
       timeline.getKeyFrames().add(kf);
       timeline.play();
@@ -373,20 +372,24 @@ public class GUI extends Application {
     });
   }
 
-  private void updateTableroTetris () {
+  private void updateTableroTetris() {
     new AnimationTimer() {
       private long lastUpdate = 0;
-      private final long velocidadDeCaida = 500_000; //500ms
+      private final long velocidadDeCaida = 500_000; // 500ms
 
       @Override
-      public void handle(long now){
-        if (now - lastUpdate >= velocidadDeCaida){
+      public void handle(long now) {
+        if (now - lastUpdate >= velocidadDeCaida) {
           updateTablero();
           lastUpdate = now;
         }
       }
 
     }.start();
+  }
+
+  public static void main(String[] args) {
+    launch(args);
   }
 
 }
